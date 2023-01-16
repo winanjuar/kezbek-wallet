612539f86d5cb9c5ced03ea61fa2a80ddc0045f3
Sugeng Winanjuar <winanjuar@gmail.com>
2023-01-17 05:26:01 +0700

    last check

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,12 @@ import {
   SwaggerCustomOptions,
   SwaggerModule,
 } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet.hidePoweredBy());
   const configService = app.get(ConfigService);
   const serviceName = configService.get<string>('APP_NAME');
   const logger = new Logger(serviceName);
